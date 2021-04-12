aef96aec7b393ca0f619c74c77b09f078b16e705
Tristonvn <tristonvn@gmail.com>
2021-04-13 00:19:11 +0200

    Complete project

--- harry_potter_sorting_hat/harry_potter_sorting_hat.cpp
+++ harry_potter_sorting_hat/harry_potter_sorting_hat.cpp
@@ -38,9 +38,9 @@ int main()
             break;
     }
 
-    std::cout << "Q2) Dawn or Dusk?\n";
-    std::cout << "1) Dawn";
-    std::cout << "2) Dusk";
+    std::cout << "Q3) Dawn or Dusk?\n\n";
+    std::cout << "1) Dawn\n";
+    std::cout << "2) Dusk\n";
     std::cin >> answer2;
 
     switch (answer2) {
@@ -53,4 +53,83 @@ int main()
         hufflepuff++;
         break;
     }
+
+    std::cout << "Q3) Which kind of instrument most pleases your ear?\n\n";
+    std::cout << "1) The violin\n";
+    std::cout << "2) The trumpet\n";
+    std::cout << "3) The piano\n";
+    std::cout << "4) The drum\n";
+    std::cin >> answer3;
+
+    switch (answer3) {
+    case 1:
+        slytherin++;
+        break;
+    case 2:
+        hufflepuff++;
+        break;
+    case 3:
+        ravenclaw++;
+        break;
+    case 4:
+        gryffindor++;
+        break;
+    default:
+        std::cout << "Invalid input";
+        break;
+    }
+
+    std::cout << "Q4) Which road tempts you most?\n\n";
+    std::cout << "1) The wide, sunny grassy lane\n";
+    std::cout << "2) The narrow, dark, lantern-lit alley\n";
+    std::cout << "3) The twisting, leaf-strewn path through woods\n";
+    std::cout << "4) The cobbled street lined (ancient buildings)\n";
+    std::cin >> answer1;
+
+    switch (answer1) {
+    case 1:
+        hufflepuff++;
+        break;
+    case 2:
+        slytherin++;
+        break;
+    case 3:
+        gryffindor++;
+        break;
+    case 4:
+        ravenclaw++;
+        break;
+    default:
+        std::cout << "Invalid input";
+        break;
+    }
+
+    int max = 0;
+    std::string house;
+
+    if (gryffindor > max) {
+        
+        max = gryffindor;
+        house = "Gryffindor";
+    }
+
+    if (hufflepuff > max) {
+
+        max = hufflepuff;
+        house = "Hufflepuff";
+    }
+
+    if (slytherin > max) {
+
+        max = slytherin;
+        house = "Slytherin";
+    }
+
+    if (ravenclaw > max) {
+
+        max = ravenclaw;
+        house = "Ravenclaw";
+    }
+
+    std::cout << house << "!\n";
 }
